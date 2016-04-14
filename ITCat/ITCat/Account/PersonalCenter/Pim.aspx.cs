aa7ced5fd9c17f43035110c4aea5ce11efcfb7fa
汪志强 <1415620933@qq.com>
2016-04-14 11:17:05 +0800

    pim.asp.cs

---
+++ ITCat/ITCat/Account/PersonalCenter/Pim.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace iTCat.Account.PersonalCenter
+{
+    public partial class Pim : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
