6159f980a9331172419c1fcffdd73bcb3becfc61
雪峰 晏 <1029664740@qq.com>
2016-04-21 17:17:46 +0800

    edd

---
+++ ITCat/BLL/BNewTicketClass.cs
@@ -0,0 +1,37 @@
+﻿using Model.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+     public class BNewTicketClass
+    {
+        //新建票种BLL代码
+        public NewTicketClassDAL NewTicketClassDAL { get; set; }
+        NewTicketClassDAL td = new NewTicketClassDAL();
+        public bool Ticket(
+            string ticketsID ,
+            string Ticket_type, 
+            string tickets_name,
+            string ticketsPrices,
+            string Tickets_illustrate,
+            string OrderDateD,
+            string SpotID, 
+            string Ticket_time
+            )
+        {
+            return td.Ticket(
+             ticketsID,
+             Ticket_type,
+             tickets_name,
+             ticketsPrices,
+             Tickets_illustrate,
+             OrderDateD,
+             SpotID,
+             Ticket_time);
+        }
+    }
+}
