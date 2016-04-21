ccf7c2dadb6136d271110c5cef75498feb2e1265
汪志强 <1415620933@qq.com>
2016-04-21 10:59:54 +0800

    PimHandler1.ashx

---
+++ ITCat/ITCat/Handler/PimHandler1.ashx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace iTCat.Handler
+{
+    /// <summary>
+    /// PimHandler1 的摘要说明
+    /// </summary>
+    public class PimHandler1 : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Hello World");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
