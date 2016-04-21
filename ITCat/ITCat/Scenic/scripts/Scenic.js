ec6cb091961310bd5ecf76486db00e5957b5b834
雪峰 晏 <1029664740@qq.com>
2016-04-21 10:22:01 +0800

    修改Scnic

--- ITCat/ITCat/Scenic/scripts/Scenic.js
+++ ITCat/ITCat/Scenic/scripts/Scenic.js
@@ -1,1 +1,25 @@
 ﻿
+
+function submitForm() {
+    $('#ff').form('submit');
+    $("#submitForm").click(
+     function () {
+         $.post(
+             "../../../Handler.ashx",
+        {
+            ScenicspotID: $("#txtScenicspotID").val(),
+  
+        },
+         function (data) {
+             if (data == "1") {
+                 alert("提交成功");
+             }
+             else {
+                 alert("提交失败");
+             }
+         }
+        );
+     }
+             );
+
+}
