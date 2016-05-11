429f120fc99f3790dfba438c25bac8de5310f73e
安琨 杨 <kurosu_cat@hotmail.com>
2016-05-11 16:01:01 +0800

    index bll

--- ITCat/Model/DAL/IndexDal.cs
+++ ITCat/Model/DAL/IndexDal.cs
@@ -1,33 +1,20 @@
-﻿using MSSQLDAL;
-using System;
-using System.Collections.Generic;
+﻿using System;
+using MSSQLDAL;
 using System.Data;
 using System.Data.Common;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Model.DAL
 {
-    class IndexDal
+    public class IndexDal
     {
         public String Index()
         {
             DbHelper db = new DbHelper();
 
             DbCommand cmd = db.GetStoredProcCommond("sp_Index");
-            
-            db.AddReturnParameter(cmd, "@retValue", DbType.Int32);
+            DataTable dt = db.ExecuteDataTable(cmd);
 
-            db.ExecuteNonQuery(cmd);
-
-
-            string result = cmd.Parameters["@retValue"].Value.ToString();
-
-            if (result == "-1")
-                return "-1";
-            else
-                return result;
+            return "1";
         }
     }
 }
