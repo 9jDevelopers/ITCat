7eb8e3aa8c259e218a1bf181923a03b4f99ca855
安琨 杨 <kurosu_cat@hotmail.com>
2016-05-12 08:36:56 +0800

    index js

--- ITCat/ITCat/Index/js/Index.js
+++ ITCat/ITCat/Index/js/Index.js
@@ -1,1 +1,15 @@
-﻿
+﻿$(function ()
+{
+    //给ID为btn-login的标签设置点击事件
+    $(document).ready(function ()
+    {
+        $.post("/Handler/IndexHandler.ashx",
+                //data：AccountHandler中返回的值
+                function (data)
+                {
+
+                }
+                //回调函数
+            );
+    });
+});
