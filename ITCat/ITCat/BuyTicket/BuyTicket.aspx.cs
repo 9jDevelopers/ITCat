488a40d073313186ad2464850afc7fc2d0a79ef2
江家强 <361890054@qq.com>
2016-05-18 08:16:24 +0800

    同步提交

--- ITCat/ITCat/BuyTicket/BuyTicket.aspx.cs
+++ ITCat/ITCat/BuyTicket/BuyTicket.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace iTCat.BuyTicket
 {
-    public partial class BuyTicket11 : System.Web.UI.Page
+    public partial class BuyTicket : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
