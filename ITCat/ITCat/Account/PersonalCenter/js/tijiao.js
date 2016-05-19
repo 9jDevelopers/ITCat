1cff2fc9fea87640c3f7c0eb10b891a0e9bf2f11
帅的人 <@DESKTOP-3H0RQHL>
2016-05-19 17:47:42 +0800

    调用信息数据

--- ITCat/ITCat/Account/PersonalCenter/js/tijiao.js
+++ ITCat/ITCat/Account/PersonalCenter/js/tijiao.js
@@ -1,5 +1,26 @@
 ﻿   
-       function submitForm() {            
+$(document).ready(function ()
+{
+    $.post(
+                   "../../Handler/transferpimone.ashx",
+              {
+                  Action:"userInfo",
+                  UserID: 1,        
+              },
+        function (data) 
+        {
+            if (data == "1") {
+                alert("调用成功");
+            }
+            else 
+            {
+                alert("调用失败");
+            }
+        }
+          )
+},
+       function submitForm() 
+       {            
            $('##frmMain').form('submit',
            $("#submitForm").click(
             function () {
@@ -20,6 +41,7 @@
                    Head_Portrait: $("#txtHead_Portrait").val(),
                     
                },
+
                 function (data) {
                     if (data == "1") {
                         alert("提交成功");
