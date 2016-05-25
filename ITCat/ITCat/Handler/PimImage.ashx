2131e7ddcf746ba300d4f5cc7ede317cb4335794
帅的人 <@DESKTOP-3H0RQHL>
2016-05-25 14:52:43 +0800

    图片调用

---
+++ ITCat/ITCat/Handler/PimImage.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="PimImage.ashx.cs" Class="iTCat.Handler.PimImage" %>
---
+++ ITCat/ITCat/Handler/PimImage.ashx.cs
@@ -0,0 +1,34 @@
+﻿using BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace iTCat.Handler
+{
+    /// <summary>
+    /// PimImage 的摘要说明
+    /// </summary>
+    public class PimImage : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            string result;
+            BPim a = new BPim();
+            //调用数据
+            string userid = context.Request["UserID"];
+            int UserID = int.Parse(userid);
+            result = a.Information(UserID);
+            context.Response.Write(result);
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
