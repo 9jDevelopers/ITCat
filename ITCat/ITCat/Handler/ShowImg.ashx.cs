00e5113bdd673ecefed3b7dace1d27eb1ba135ce
安琨 杨 <kurosu_cat@hotmail.com>
2016-05-26 17:30:15 +0800

    showimg

---
+++ ITCat/ITCat/Handler/ShowImg.ashx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BLL;
+
+namespace iTCat.Handler
+{
+    /// <summary>
+    /// ShowImg 的摘要说明
+    /// </summary>
+    public class ShowImg : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            BIndex bi = new BIndex();
+            string AttractionsID= context.Request["AttractionsID"];
+            string result = bi.GetImage(AttractionsID);
+
+            if (result != null)
+            {
+                context.Response.Write(result);
+            }
+
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
