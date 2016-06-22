2d2e9cd6e3e941470bc49af63a707b78480f8ffd
QG <@DESKTOP-PVGAM3P>
2016-06-22 16:37:57 +0800

    订单

---
+++ ITCat/ITCat/Handler/OrderHandler.ashx.cs
@@ -0,0 +1,34 @@
+﻿using BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace iTCat.Handler
+{
+    /// <summary>
+    /// OrderHandler 的摘要说明
+    /// </summary>
+    public class OrderHandler : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            string result;
+            BPim a = new BPim();
+            //调用数据session接收userid
+            string userid = context.Session["userID"].ToString();
+            int UserID = int.Parse(userid);
+            result = a.order(UserID);
+            context.Response.Write(result);
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
